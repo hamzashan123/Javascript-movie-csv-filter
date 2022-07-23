c5b2bddbfb0236fc6734550bc958b4d3b16e1e5b
hamzashan <36412008+hamzashan123@users.noreply.github.com>
2022-07-23 01:07:43 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-d3.csv("movies.csv").then(function (data) {
-  // console.log(data);
+// d3.csv("movies.csv").then(function (data) {
+  d3.csv("job.csv").then(function (data) {
+   console.log(data);
 
   var movies = data;
 
@@ -18,7 +19,7 @@ d3.csv("movies.csv").then(function (data) {
     var inputValue = inputElement.property("value").toLowerCase().trim();
 
     // console.log(inputValue.length);
-    // console.log(movies);
+     console.log(movies);
     if (inputValue.length < 6){
       d3.select("p").classed('noresults2', true).html("<center><strong>Please try using more than 5 characters to avoid too many results!</strong>")
       inputValue = "Something to give no results"
@@ -28,14 +29,15 @@ d3.csv("movies.csv").then(function (data) {
     if (filteredData.length === 0 && inputValue !== "Something to give no results"){
       d3.select("p").classed('noresults', true).html("<center><strong>No results. Please check your spelling!</strong>")
     }
-    output = _.sortBy(filteredData, 'avg_vote').reverse()
+    output = _.sortBy(filteredData, 'cve_id').reverse()
 
     for (var i = 0; i < filteredData.length; i++) {
-      // console.log(output[i]['original_title'])
+       console.log(output[i])
       // console.log(output[i]['avg_vote'])
       // d3.select("tbody>tr>td").text(output[i]['original_title']);
-      d3.select("tbody").insert("tr").html("<td>"+[i+1]+"</td>"+"<td>"+"<a href=https://www.imdb.com/title/"+output[i]['imdb_title_id']+" target='_blank'>"+(output[i]['original_title'])+"</a>"
-      + "</td>" +"<td>" +(output[i]['avg_vote'])+"</td>" +"<td>" +(output[i]['year'])+"</td>"  +"<td>" +(output[i]['director'])+"</td>"+"<td>" +(output[i]['description'])+"</td>") }
+    //  d3.select("tbody").insert("tr").html("<td>"+[i+1]+"</td>"+"<td>"+"<a href=https://www.imdb.com/title/"+output[i]['imdb_title_id']+" target='_blank'>"+(output[i]['original_title'])+"</a>"
+      //+ "</td>" +"<td>" +(output[i]['avg_vote'])+"</td>" +"<td>" +(output[i]['year'])+"</td>"  +"<td>" +(output[i]['director'])+"</td>"+"<td>" +(output[i]['description'])+"</td>")
+    }
   };
   window.resizeTo(screen.width,screen.height)
 
